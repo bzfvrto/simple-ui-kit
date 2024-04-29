257f91d490c72e8518e7361a83530bf49c89ecd3
Basile Favretto <bazil.f@gmail.com>
2024-04-29 18:44:27 +0200

    Btn and input bckg & text color

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./Button.module.css";
+import { ThemeContext } from "../Theme/ThemeContext";
 
 function Button(props) {
+    const theme = useContext(ThemeContext);
+    let btnStyle = {
+        color:
+            theme.color === "dark"
+                ? "white"
+                : theme.color === "light"
+                ? "black"
+                : "#6d28d9",
+    };
     return (
-        <button className={styles.btn} {...props}>
+        <button className={styles.btn} {...props} style={btnStyle}>
             {props.children}
         </button>
     );
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./Input.module.css";
+import { ThemeContext } from "../Theme/ThemeContext";
 
 function Input({ type = "text", label, placeholder }) {
+    const theme = useContext(ThemeContext);
+    let textColor = {
+        color:
+            theme.color === "dark"
+                ? "white"
+                : theme.color === "light"
+                ? "black"
+                : "#6d28d9",
+        backgroundColor:
+            theme.color === "light"
+                ? "white"
+                : theme.color === "dark"
+                ? "#1e293b"
+                : "#f59e0b",
+    };
     return (
         <div className={styles.inputContainer}>
             <label>{label}</label>
@@ -9,6 +25,7 @@ function Input({ type = "text", label, placeholder }) {
                 className={styles.input}
                 type={type}
                 placeholder={placeholder}
+                style={textColor}
             />
         </div>
     );
